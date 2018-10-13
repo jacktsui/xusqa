afeaaebcb7786b3f491f067d2356d979415ffade
jacktsui <355088586@qq.com>
2018-10-13 23:02:48 +0800

    fix message width

--- xusqa.user.js
+++ xusqa.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         有道搜题录题助手
 // @namespace    jacktsui
-// @version      1.3.144
+// @version      1.3.145
 // @description  有道搜题,录题员助手(一键领取任务,广场任务数量角标显示,任务报告,一键整理,定位答案,框选截图,放大镜,题目保存和恢复,优化系统行为等)
 // @author       Jacktsui
 // @copyright    © 2018, 徐。355088586@qq.com
@@ -52,7 +52,7 @@
 (function() {
     'use strict';
 
-const ver = '1.3.144'
+const ver = '1.3.145'
 
 // 扩展版本号代理
 let ver_kfe = '0.0.000'
@@ -2597,6 +2597,9 @@ if (O.optimizeQJudgeShow){
 
 // add css sheet to header, not comment
 util.addStyle(util.cmt(function(){/*!CSS
+.el-message {
+    width: fit-content;
+}
 .el-message__content thead {
     border-bottom: 1px solid;
 }
